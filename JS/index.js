157be90782777b3a22a21fff0c7db4805542e7d7
milson389 <mugetsu3008@gmail.com>
2020-08-07 19:57:25 +0700

    Dicoding Pemrograman Web Dasar

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
